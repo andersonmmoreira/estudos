b223a821c31b7e8cde8279464464d194ca4cc594
Anderson de M Moreira <166535957+andersonmmoreira@users.noreply.github.com>
2024-07-08 09:53:23 -0300

    Estudando JS

---
+++ fullstackB7WEB/JavaScript/modulo_02/aula_03/switch.js
@@ -0,0 +1,16 @@
+let profession = "";
+
+ switch (profession) {
+    case 'fiscal':
+        console.log ('Sua camisa será VERDE');
+        break;
+    case 'bombeiro':
+        console.log ('Sua camisa será VERMELHA');
+        break;
+    case 'policial':
+        console.log ('Sua camisa será AZUL');
+        break;
+    default:
+        console.log ('Sua camisa será PRETA'); 
+        break;
+ }
