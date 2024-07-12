83ab026c7d4e5cd3d9ff89e559c89b3dee1fe630
Anderson de M Moreira <166535957+andersonmmoreira@users.noreply.github.com>
2024-07-12 07:43:03 -0300

    up

---
+++ fullstackDC/JavaScript/exercicio/script.js
@@ -0,0 +1,78 @@
+// Crie uma variável a e uma variável b e atribua valores numéricos a elas. Calcule
+// a soma desses dois números e armazene o resultado em uma terceira variável
+// soma.
+
+let a = 10;
+let b = 20;
+let soma = a + b;
+console.log("A soma de", a, "e", b, "é", soma);
+
+// Declare uma variável c e atribua um valor numérico a ela. Em seguida,
+// multiplique c por 10 e armazene o resultado em resultado.
+
+let c = 5;
+let resultado = c * 10;
+console.log("O resultado de multiplicar", c, "por 10 é", resultado);
+
+// Defina duas variáveis x e y com valores diferentes. Subtraia y de x e armazene o
+// resultado em subtracao.
+
+let x = 20;
+let y = 10;
+let subtracao = x - y;
+console.log("A subtração de", y, "de", x, "é", subtracao);
+
+// Atribua um valor inicial a uma variável numero. Incremente esse valor em 5
+// unidades utilizando o operador de incremento (++).
+
+let numero = 10;
+numero++;
+console.log("O valor de numero após o incremento é", numero);
+
+// Crie uma variável d e outra e com valores numéricos. Divida d por e e armazene
+// o resultado em resultadoDivisao.
+
+let d = 20;
+let e = 4;
+let resultadoDivisao = d / e;
+console.log("O resultado da divisão de", d, "por", e, "é", resultadoDivisao);
+
+// Declare uma variável f com um valor numérico. Eleve f ao quadrado e
+// armazene o resultado em quadrado.
+
+let f = 5;
+let quadrado = f ** 2;
+console.log("O quadrado de", f, "é", quadrado);
+
+// Atribua um número inicial a uma variável g. Multiplique g por 2 e depois some
+// 10 ao resultado.
+
+let g = 5;
+g = g * 2 + 10;
+console.log("O resultado da multiplicação de", g, "por 2 e soma de 10 é", g);
+
+// Declare uma variável h e uma i com valores diferentes. Verifique se h é maior
+// que i utilizando um operador de comparação e armazene o resultado booleano
+// em uma variável maior.
+
+let h = 15;
+let i = 10;
+let maior = h > i;
+console.log("É verdade que", h, "é maior que", i, "?", maior);
+
+// Crie uma variável j e outra k com valores numéricos. Calcule o resto da divisão
+// de j por k e armazene o resultado em resto.
+
+let j = 17;
+let k = 5;
+let resto = j % k;
+console.log("O resto da divisão de", j, "por", k, "é", resto);
+
+// Defina uma variável l com um valor numérico. Verifique se l é igual a 0
+// utilizando um operador de igualdade e armazene o resultado booleano em
+// igualZero.
+
+let l = 0;
+let igualZero = l === 0;
+console.log("É verdade que", l, "é igual a 0?", igualZero);
+
