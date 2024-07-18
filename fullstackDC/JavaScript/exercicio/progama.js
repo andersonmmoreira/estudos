28a7448d6da10f04413740d5ad698f8279431da4
Anderson de M Moreira <166535957+andersonmmoreira@users.noreply.github.com>
2024-07-18 13:32:52 -0300

    up

---
+++ fullstackDC/JavaScript/exercicio/progama.js
@@ -0,0 +1,11 @@
+// Recebe os 4 valores do usuário
+let valor1 = parseInt(prompt("Digite o primeiro valor:"));
+let valor2 = parseInt(prompt("Digite o segundo valor:"));
+let valor3 = parseInt(prompt("Digite o terceiro valor:"));
+let valor4 = parseInt(prompt("Digite o quarto valor:"));
+
+// Soma os valores
+let total = valor1 + valor2 + valor3 + valor4;
+
+// Exibe o resultado em um alerta
+alert("OLÁ, SEU TOTAL É " + total);
